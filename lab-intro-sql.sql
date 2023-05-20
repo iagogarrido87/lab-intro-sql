ec73d7454d8b43f8c4934fa6c07bdafc0758104f
Iago Garrido <garridorodrigueziago@gmail.com>
2023-05-20 15:56:42 +0200

    delivery of answers

---
+++ lab-intro-sql.sql
@@ -0,0 +1,9 @@
+use sakila;
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+SELECT title FROM film;
+SELECT DISTINCT language.name AS language FROM language;
+SELECT COUNT(*) AS store_count FROM store;
+SELECT COUNT(*) AS employee_count FROM staff;
+SELECT first_name FROM staff;
